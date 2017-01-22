d659f8d4e66ea978ae66bc40e8b2cec8ac274c21
sek8888 <sek8888@mail.ru>
2017-01-23 02:11:06 +0400

    Update urls.py

--- yomamabot/urls.py
+++ yomamabot/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('fb_yomamabot.urls')),
+    url(r'^fb/', include('fb_yomamabot.urls')),
 ]
