e8ae89ce322cdf9849bd397f8c454a87c3b4ce6d
sek8888 <sek8888@mail.ru>
2017-01-23 02:30:05 +0400

    Update views.py

--- fb_yomamabot/views.py
+++ fb_yomamabot/views.py
@@ -2,9 +2,10 @@ from django.views import generic
 from django.http.response import HttpResponse
 # Create your views here.
 
-class YoMamaBotView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('qwer')
-    def index(request):
+def index(request):
     # return HttpResponse('Hello from Python!')
     return HttpResponse('qwer')
+class YoMamaBotView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('qwer2')
+
